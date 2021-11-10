f3be0408d810c0f555f6cd77cb306abbaa64b563
rab-tab <sdetcode@gmail.com>
2021-11-11 00:45:24 +0530

    First commit

---
+++ src/main/java/com/selenium/spring/seleniumspringboot/SeleniumSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.selenium.spring.seleniumspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleniumSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selenium/spring/seleniumspringboot/SeleniumSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selenium.spring.seleniumspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
