0e89f7969523f800b36b94703214fb0c547ef365
rab-tab <sdetcode@gmail.com>
2021-11-11 15:59:01 +0530

    webdriver wait config

---
+++ src/main/java/com/selenium/spring/seleniumspringboot/config/WebDriverWaitConfig.java
@@ -0,0 +1,17 @@
+package com.selenium.spring.seleniumspringboot.config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class WebDriverWaitConfig {
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public WebDriverWait webdriverWait(WebDriver driver){
+        return new WebDriverWait(driver, 200);
+    }
+}
